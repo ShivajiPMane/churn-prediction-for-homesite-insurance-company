98b8b1e3a1245dc004473f419f4cdcfef33a6d38
Shivaji Mane <maneshiva91@gmail.com>
2023-03-18 18:21:26 +0530

    removed create_app() function

--- app.py
+++ app.py
@@ -4,35 +4,32 @@ import joblib
 from pre_processing import Preprocess
 
 
-def create_app():
-    app = Flask(__name__)
+app = Flask(__name__)
 
-    model = joblib.load("files/xgb_clf_model.pkl")
-
-
-    @app.route("/", methods=['GET', 'POST'])
-    def index():
-        return render_template("home.html")
+model = joblib.load("files/xgb_clf_model.pkl")
 
 
-    @app.route('/data', methods=['GET', 'POST'])
-    def data():
-        if request.method == 'POST':
-            f = request.files['file']
-            file = pd.read_csv(f)
-            pp = Preprocess()
-            data_pt = pp.preprocessing_datapoint(file)
-            prediction = model.predict(data_pt)[0]
-
-        if prediction == 1:
-            op_str = "Churned : This Customer is likely tobe purchased a quoted insurance plan."
-        else:
-            op_str = "Not Churned : This Customer is might not purchase a quoted insurance plan"
-
-        return render_template("data.html", data=op_str)
+@app.route("/", methods=['GET', 'POST'])
+def index():
+    return render_template("home.html")
 
 
-    if __name__ == "__main__":
-        app.run()           # debug=True
+@app.route('/data', methods=['GET', 'POST'])
+def data():
+    if request.method == 'POST':
+        f = request.files['file']
+        file = pd.read_csv(f)
+        pp = Preprocess()
+        data_pt = pp.preprocessing_datapoint(file)
+        prediction = model.predict(data_pt)[0]
 
-    return app
+    if prediction == 1:
+        op_str = "Churned : This Customer is likely tobe purchased a quoted insurance plan."
+    else:
+        op_str = "Not Churned : This Customer is might not purchase a quoted insurance plan"
+
+    return render_template("data.html", data=op_str)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
